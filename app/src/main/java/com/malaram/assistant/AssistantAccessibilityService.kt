4f55660c37aa78650b23fee2d6e6091ae2b156b8
malaramofficial <malaramofficial@gmail.com>
2026-09-19 20:06:20 +0530

    Add accessibility service

---
+++ app/src/main/java/com/malaram/assistant/AssistantAccessibilityService.kt
@@ -0,0 +1,12 @@
+package com.malaram.assistant
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class AssistantAccessibilityService : AccessibilityService() {
+    companion object { var instance: AssistantAccessibilityService? = null }
+    override fun onServiceConnected() { super.onServiceConnected(); instance = this }
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    override fun onInterrupt() {}
+    override fun onDestroy() { instance = null; super.onDestroy() }
+}
